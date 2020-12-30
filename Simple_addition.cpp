71a5500248f69b07658d658e472b942046e7fa89
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2020-12-30 19:10:00 +0530

    Day 1

---
+++ Simple_addition.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a, b;
+    int sum;
+
+    cout<<"Enter the first number : "<<endl;
+    cin>>a;
+    cout<<"Enter the second number : "<<endl;
+    cin>>b;
+
+    sum = a + b;
+
+    cout<<"Sum = "<<sum<<endl;
+}
