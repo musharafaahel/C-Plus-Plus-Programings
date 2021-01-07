36f9d4d412f7aa181347b05fea538dd33870f88f
Musharaf Aahel <49841802+musharafaahel@users.noreply.github.com>
2021-01-07 14:10:55 +0530

    DAY 7

---
+++ cast_operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main() {
+    int a = 25;
+    cout<<"--------------------------------"<<endl;
+    cout<<"cast operator"<<endl;
+    cout<<"--------------------------------"<<endl;
+
+    cout<<char(a)<<endl;
+    
+    return 0;
+}
